906c57504923ca92f23924dc9fe10f571224e430
james-d12 <james_d02@protonmail.com>
2020-08-16 13:42:24 +0000

    Add files via upload

---
+++ makefile
@@ -0,0 +1,4 @@
+install-pkg:
+	sh pkgs/install-packages.sh 
+install-pkg-aur:
+	sh pkgs/install-packages-aur.sh
---
+++ pkgs/install-packages-aur.sh
@@ -0,0 +1,11 @@
+#!bin\sh
+
+mkdir temp
+cd temp
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
+
+yay -S --needed - < pkgs/pkgaurlist.txt
