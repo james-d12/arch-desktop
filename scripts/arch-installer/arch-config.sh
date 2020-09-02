f318c64521e3d24decf75956242a21a506e9bf93
james <james_d02@protonmail.com>
2020-09-02 14:13:14 +0100

    Added prompts

--- scripts/arch-installer/arch-config.sh
+++ scripts/arch-installer/arch-config.sh
@@ -4,33 +4,12 @@
 
 ## Desktop Environment Parameters
 
-desktopenvironment="gnome-minimal"   
-# SELECT DESKTOP ENVIRONMENT, OPTIONS ARE:
-# - gnome                   | Installs the Gnome group package.
-# - gnome-minimal           | Install Gnome with a minimal set of applications
-# - kde                     | Installs KDE plasma packages
-# - kde-minimal             | Installs KDE plasma with a minimal set of packages
-# - xfce                    | Installs XFCE DE and packages.
-# - xfce-minimal            | Installs XFCE DE with a minimal set of packages
-# - custom                  | Installs custom DE and packages
-# - (empty)                 | If string is empty will not install any Desktop Environment.
-
 ## Drive Parameters
 
 drive="sda"                # DRIVE NAME
 encryptedswapsize="4096"   # IF ENCRYPTION: Encrypted SWAP size (MB)
 ssd='NO'                   # IS DRIVE SSD? YES | NO          
-
-system=""              
-encrypted=""             
-kernel=""         
-microcode=""    
-user=""                
-locale=""            
-region=""            
-city=""              
-hostname=""         
-
+      
 # predefined parameters DO NOT CHANGE!
 MSGCOLOUR='\033[0;33m'
 PROMPTCOLOUR='\033[0;32m'
